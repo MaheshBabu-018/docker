a3b272c18128b72cbdb94a1660d991b2e34e6f98
Maheshbabu <mamahesh585@gmail.com>
2025-09-08 14:34:42 +0530

    Docker task5

--- task5/postgre.py
+++ task5/postgre.py
@@ -1,7 +1,18 @@
-import psycopg2, time
-time.sleep(5)  # give Postgres a moment on first run
-conn = psycopg2.connect(
-    host="my-postgres", database="task_db", user="admin", password="secure123"
-)
-print("Connected to the database!")
-conn.close()
+import psycopg2
+import time
+
+# Wait a bit for Postgres to be ready
+time.sleep(5)
+
+try:
+    conn = psycopg2.connect(
+        host="my-postgres",
+        database="mydb",
+        user="maheshbabu",
+        password="Srack@585"
+    )
+    print("Connected to the database!")
+    conn.close()
+
+except Exception as e:
+    print("Connection failed:", e)
